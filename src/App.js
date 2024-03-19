b7bb8bf99e8e923d6f2a323300efd2de4fd7b348
aryanankit <a.arya8279@gmail.com>
2024-03-19 19:49:15 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactFlow from "reactflow";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import 'reactflow/dist/style.css';
+
+const initialNodes = [
+  { id: '1', position: { x:0, y:0 }, data: { label: 'Anton' }},
+  { id: '2', position : { x:0, y:100 }, data: { label: 'Son of Anton' }}
+];
+
+const initialEdges = [
+  {id: 'e1-2', source : '1' , target : '2'}
+]
+
+
+const App = () => {
+  return ( <div style={{ width: '100vw', height: '100vh'}}>
+    <ReactFlow nodes={initialNodes} edges={initialEdges} />
+  </div>
   );
 }
 
