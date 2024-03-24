40e898f7101f40875eb70203af006530f44e093d
aryanankit <a.arya8279@gmail.com>
2024-03-24 20:03:24 +0530

    Added the functionality for connecting nodes using ui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 
-import Main from './components/main';
+import { ReactFlowProvider } from 'reactflow';
+import Flow from './components/Flow';//eslint-disable-next-line 
 import 'reactflow/dist/style.css';
 
 const App = () => {
-  const initialNodes = [
-    { id: '1', position : { x:0, y:0 }, data: { label: 'Anton' }},
-    { id: '2', position : { x:100, y:100 }, data: { label: 'Son of Anton' }},
-    { id: '3', position : { x:0, y:200 }, data: { label: 'Daughter of Anton' }}
-  ];
-  
-  const initialEdges = [
-    {id: 'e1-2', source : '1' , target : '2'},
-    {id: 'e1-3', source : '1' , target : '3'}
-  ]
-  return (<div>
-    <Main initialEdges={initialEdges} initialNodes={initialNodes} />
+  return (<div  style={{ height: 800 }}>
+    <ReactFlowProvider>
+      <Flow  />
+    </ReactFlowProvider>
   </div>
   )
   
